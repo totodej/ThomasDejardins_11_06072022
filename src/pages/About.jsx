e945155cf3517d30e4c1acfa50a5a47e5dec9b68
Thomas Dejardins <tdejardins@gmail.com>
2022-08-01 15:03:32 +0200

    feat(About.jsx): add About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import BackgroundAbout from "../components/About/BackgroundAbout";
+import DescriptionsAbout from "../components/About/DescriptionsAbout";
+
+function About() {
+  return (
+    <div>
+      <BackgroundAbout />
+      <DescriptionsAbout />
+    </div>
+  );
+}
+
+export default About;
