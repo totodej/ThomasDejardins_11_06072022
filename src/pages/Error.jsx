352509b7fe70ac1b822428b019cac424f220b274
Thomas Dejardins <tdejardins@gmail.com>
2022-08-01 15:04:35 +0200

    feat(Error.jsx): add Error 404 page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,16 @@
+import "../style/Error.css";
+import { Link } from "react-router-dom";
+
+function Error() {
+  return (
+    <div className="error-404">
+      <h2>404</h2>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="link-error">
+        Retourner sur la page d'acceuil
+      </Link>
+    </div>
+  );
+}
+
+export default Error;
